d6a981bd227edd807b5d808b6efcfa6bb3be4855
Igor <garkol@bk.ru>
2022-08-09 21:48:45 +0300

    task one

---
+++ HomeWork06_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+
+Console.Write("Введите количество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 1; i <= M; i++)
+{
+    Console.Write("Введите число " + i + ": ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine("Пользователь ввёл "+ count + " положительных чисел");
